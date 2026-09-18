00d60c78fa01f762c1d387962b6595e9b83973ea
chramiq <720d70d1-e670-4c01-b74a-b701655a9e2b@slmail.me>
2026-09-18 13:20:12 +0200

    Release 0.15.0

--- plugin/src/main/java/vpmcp/RouterPlugin.java
+++ plugin/src/main/java/vpmcp/RouterPlugin.java
@@ -30,7 +30,7 @@ import vpmcp.vp.VpLog;
  */
 public final class RouterPlugin implements VPPlugin {
 
-    private static final String VERSION = "0.14.2";
+    private static final String VERSION = "0.15.0";
     private static final String SCHEMA_VERSION = "v18.1";
 
     private McpServer server;
